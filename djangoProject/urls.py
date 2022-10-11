511d8b4c16579a9cfb4476a1dfbfbfe93c108363
Labio07 <1348757279@qq.com>
2022-10-11 10:15:51 +0800

    second

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import app01
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',app01.views.hello),
 ]
